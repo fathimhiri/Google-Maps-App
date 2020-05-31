ec0efcb3ae79f5f0bc10a5cb26047372fbabfeec
fathi <fathimhiri10@gmail.com>
2020-05-31 05:32:05 +0100

    retrieveing data by looping through the data in the store-data.js and replacing variable inside .stores-list

--- js/index.js
+++ js/index.js
@@ -11,22 +11,43 @@
                 center: losAngelos,
                 zoom: 8
               });
+              displayStores();
             }
       
        function displayStores(){
-           
+         var storesHtml = "";
+           stores.forEach(function(store, index){
+             var adress = store.addressLines; 
+             var phone = store.phoneNumber;
+             storesHtml +=`<div class="store-container">
+              
+             <div class= "store-info-container" >
+                  <div class="store-adress">
+                     <span>${adress[0]}</span> 
+                     <span>${adress[1]}</span></div>
+                 <div class="store-phone-number">${phone}</div>
+             </div>
+
+             <div class= "store-number-container">
+               <div class="store-number">${index +1}</div>
+             </div>
+
+       </div>`
+
+           });
+           document.querySelector('.stores-list').innerHTML=storesHtml;
        }
             
             
             
             
             
-            function addMarker(location, map) {
-              var marker = new google.maps.Marker({
-    position: location,
-    map: map,
-    label: labels
-  });
-            }
+  //           function addMarker(location, map) {
+  //             var marker = new google.maps.Marker({
+  //   position: location,
+  //   map: map,
+  //   label: labels
+  // });
+  //           }
             
           
